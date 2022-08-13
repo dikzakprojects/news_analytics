a3d8cd192112a769818e72cbf927c194ac6efadf
charl vdM <charlhvdm@gmail.com>
2022-08-13 15:39:32 +0200

    Added new comment
    Just testing dev environment

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
-
+#checking for changes
 url='https://www.bbc.com/news'
 response = requests.get(url)
 
